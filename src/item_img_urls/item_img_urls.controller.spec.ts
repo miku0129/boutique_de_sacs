d708e4f29e5ed84df7b821e41a8f69435d10ad41
miku0129 <svartkatt39@gmail.com>
2023-12-25 07:55:42 +0100

    test item_img_urls.controller

--- src/item_img_urls/item_img_urls.controller.spec.ts
+++ src/item_img_urls/item_img_urls.controller.spec.ts
@@ -11,6 +11,12 @@ describe('ItemImgUrlsController', () => {
     is_main: true,
   };
 
+  const update_dto = {
+    ...create_dto,
+    url: 'updated item_img_url',
+    is_main: false,
+  };
+
   interface Item_img_url {
     id: number;
     url: string;
@@ -28,6 +34,19 @@ describe('ItemImgUrlsController', () => {
       };
       item_img_urls.push(newItemImgUrl);
       return newItemImgUrl;
+    }),
+    findAll: jest.fn(() => item_img_urls),
+    findOne: jest.fn().mockImplementation((id) => {
+      return item_img_urls.find((url) => url.id === id);
+    }),
+    update: jest.fn().mockImplementation((id, update_dto) => {
+      const foundUrl = item_img_urls.find((url) => url.id === id);
+      return { ...foundUrl, ...update_dto };
+    }),
+    remove: jest.fn().mockImplementation((id) => {
+      const item = item_img_urls.find((item) => item.id === id);
+      item_img_urls = item_img_urls.filter((user) => user.id !== id);
+      return item;
     }),
   };
 
@@ -55,6 +74,50 @@ describe('ItemImgUrlsController', () => {
         url: 'item_img_url',
         is_main: true,
       });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should get all item_img_url', async () => {
+      controller.create(create_dto);
+      controller.create(create_dto);
+      controller.create(create_dto);
+
+      const result = await controller.findAll();
+      expect(result.length).toBe(3);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find a item_img_url by id', async () => {
+      const item1 = await controller.create(create_dto);
+
+      const result = await controller.findOne(String(item1.id));
+      expect(result.url).toEqual(item1.url);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a item_img_url by id', async () => {
+      const item1 = await controller.create(create_dto);
+      const updated_item = await controller.update(
+        String(item1.id),
+        update_dto,
+      );
+
+      expect(updated_item).toEqual({
+        id: expect.any(Number),
+        url: 'updated item_img_url',
+        is_main: false,
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a item_img_url by id', async () => {
+      const item1 = await controller.create(create_dto);
+      const removedItem = await controller.remove(String(item1.id));
+      expect(removedItem.id).toEqual(item1.id);
     });
   });
 });
